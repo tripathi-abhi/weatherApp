d558761c6cc45b92f56df7a843f6485e4e00a12d
unknown <abhitripathi2312gmail.com>
2020-11-29 02:31:37 +0530

    cli agrs based node app for current weather and weather forecast

---
+++ app.js
@@ -0,0 +1,36 @@
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
+
+if (!process.argv[2]) {
+	return console.log("Address field required with CLI command");
+}
+
+const getAddress = () => {
+	let i = 2;
+	let address = "";
+	while (process.argv[i] != undefined) {
+		address += process.argv[i] + " ";
+		i++;
+	}
+	return address;
+};
+
+geocode(getAddress(), (error, { latitude, longitude, location } = {}) => {
+	if (error) {
+		return console.log("Error ", error);
+	}
+	console.log("Place : ", location);
+	forecast(
+		latitude,
+		longitude,
+		(error, { description, temperature, humidity } = {}) => {
+			if (!error) {
+				console.log(
+					`Weather description : ${description} \nTemperature : ${temperature}K \nHumidity : ${humidity}`
+				);
+			} else {
+				console.log("Error ", error);
+			}
+		}
+	);
+});
